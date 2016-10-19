5c59680e10882c36e12fda3a8d809e83ff800981
lede <lede@zaxis-studios.com>
2016-10-19 12:29:22 -0700

    Adding initial project code to repository

---
+++ js/canven-1.0.0.js
@@ -0,0 +1,386 @@
+﻿window.requestAnimFrame = (function () {
+	return window.requestAnimationFrame ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame ||
+			window.oRequestAnimationFrame ||
+			window.msRequestAnimationFrame ||
+			function (/* function */ callback, /* DOMElement */ element) {
+				window.setTimeout(callback, 1000 / 60);
+			};
+})();
+
+function Canven(config) {
+	let me = this;
+	me.canvas = null;
+	me.canvasId = "theEngine";
+	me.clearClr = '#ffffff';
+	me.ctx = null;
+	me.ctxType = '2d';
+	me.deadEntities = new Array();
+	me.entityList = new Array();
+	me.fps = 60;
+	me._fpsCnt = 0;
+	me._fpsCurr = 0;
+	me._fpsShow = true;
+	me.isReady = false;
+	me.mousePos = null;
+	me.removeByRun = false;
+	me.size = null;
+	me.splashDone = false;
+
+	me.AddEntity = (ent) => {
+		let el = me.entityList;
+		ent.Init({ id: el.length });
+		el[ent.id] = ent;
+	}
+
+	me.Clear = (ctx) => {
+		if (!me.IsValidObject(ctx)) {
+			console.error('Not a valid context');
+		}
+		let oldStyle = ctx.fillStyle;
+		ctx.fillStyle = me.clearClr;
+		ctx.fillRect(0, 0, me.size.x, me.size.y);
+
+		ctx.fillStyle = oldStyle;
+
+		return me;
+	};
+
+	me.Close = () => {
+		me.isReady = false;
+		me.entityList = new Array();
+		me.deadEntities = new Array();
+		console.log('Engine closed for business');
+	};
+
+	me.fpsUpdate = () => {
+		me._fpsCurr = me._fpsCnt;
+		me._fpsCnt = 0;
+		if (me._isReady) {
+			setTimeout(me.fpsUpdate, 1000);
+		}
+	};
+
+	me.fpsShow = (ctx) => {
+		if (!me.IsValidObject(ctx)) {
+			console.error('Context is endefined!');
+			return;
+		}
+
+		let oldStyle = ctx.fillStyle;
+		let oldFont = ctx.font;
+		ctx.fillStyle = '#909090';
+		ctx.font = '12pt Georgia';
+
+		let msg = `FPS: ${me._fpsCurr}`;
+
+		ctx.fillText(msg, 30, 25);
+
+		ctx.font = oldFont;
+		ctx.fillStyle = oldStyle;
+
+		return me;
+	};
+
+	me.HexToRGB = (hex) => {
+		let rgb = {
+			r: 0, g: 0, b: 0, toString: function () {
+				return `${this.r},${this.b},${this.b}`;
+		} };
+		if(hex[0] = '#'){
+			hex = hex.substring(1, hex.length).trim();
+		}
+
+		rgb.r = parseInt(hex.substr(0, 2), 16);
+		rgb.g = parseInt(hex.substr(2, 2), 16);
+		rgb.b = parseInt(hex.substr(4, 2), 16);
+
+		return rgb;
+	};
+
+	me.IndexEntities = () => {
+		for (let i = 0; i < me.entityList.length; ++i) {
+			me.entityList[i].id = i;
+		}
+	};
+
+	me.Init = () =>{
+		me.canvas = document.getElementById(me.canvasId);
+		if (me.canvas != null && typeof (me.canvas) != 'undefined') {
+			me.ctx = me.canvas.getContext(me.ctxType);
+			if (me.ctx != null && typeof (me.ctx) != null) {
+				me.isReady = true;
+				me.size = new Vector2D({ x: me.canvas.width, y: me.canvas.height });
+
+				setTimeout(me.fpsUpdate, 1000);
+			}
+		} else {
+			console.error(`Canvas #${me.canvasId} was not found in document`);
+			return;
+		}
+
+		// Connect to mouse tracking
+		me.mousePos = me.NewVector2D(0, 0);
+		document.onmousemove = me.MouseMove;
+
+		me.Splash();
+		return me;
+	};
+
+	me.IsValidObject = (obj) => {
+		return typeof (obj) != 'undefined' && obj != null;
+	};
+
+	me.MouseMove = (e) => {
+		e = e || window.event;
+
+		me.mousePos.x = e.clientX;
+		me.mousePos.y = e.clientY;
+	};
+
+	me.NewCollider = (config) => {
+		return new Collider(config);
+	};
+
+	me.NewEntity = (config) => {
+		return new Entity(config);
+	};
+
+	me.NewVector2D = (x, y) => {
+		return new Vector2D({ x: x, y: y });
+	};
+
+	me.Physics = () => {
+		// Run physics simulation
+		for (let i = 0; i < me.entityList.length; ++i) {
+			let ent = me.entityList[i];
+			if (typeof (ent.collider) != 'undefined' && ent.collider != null) {
+				// Need to check this will all entities
+				for (let j = i + 1; j < me.entityList.length; ++j) {
+					let other = me.entityList[j];
+					// Check if there was a hit
+					if (ent.collider.CheckHit(other)) {
+						// Hit detrected so tell the colliders to handle it
+						console.log(`Hit between ${ent.id} and ${other.id}`);
+						ent.collider.Collision(other);
+						other.collider.Collision(ent);
+					}
+				}
+			}
+		}
+	};
+
+	me.RemoveEntity = (entity) => {// Sorry fokes this is still O = n
+		if (me.removeByRun) {
+			// Check if we have more then one entity to delete
+			if (me.deadEntities.length > 1) {
+				// Going the slow route to remove entities
+				for (let i = 0; i < me.deadEntities.length; ++i) {
+					// If I have more than one entity to remove this will get sticky
+					let idx = me.deadEntities[i].id;
+					for (let j = 0; j < me.entityList.length; ++i) {
+						if (me.entityList[j].id == idx) {
+							// Remove entity and stop looking
+							me.entityList.splice(j, 1);
+							break;
+						}
+					}
+				}
+				// Re index the entity list
+				me.IndexEntities();
+				me.deadEntities = new Array();
+			} else if (me.deadEntities.length == 1) {
+				// Yeah only one to remove
+				let idx = me.deadEntities[0].id;
+				// Remove entity
+				me.entityList.splice(idx);
+				me.IndexEntities();
+				me.deadEntities = new Array();
+			}
+			me.removeByRun = false;
+		} else if(me.IsValidObject(entity)) {
+			// Don't just remove an entity until the proper time in the main game loop
+			// So for now going to add it to the delete pile
+			me.deadEntities[me.deadEntities.length] = entity;
+		}
+	};
+
+	me.Run = () => {
+		if (!me.splashDone) {
+			setTimeout(me.Run, 500);
+			return;
+		}
+		if(me.isReady){
+			me.Clear(me.ctx);
+			if (me._fpsShow) {
+				me.fpsShow(me.ctx);
+			}
+			let deltaTime = 1.0;
+			for (let i = 0; i < me.entityList.length; ++i) {
+				me.entityList[i].Draw(me.ctx);
+				me.entityList[i].Move(deltaTime);
+			}
+
+			// Run the physics simulation
+			me.Physics();
+
+			++me._fpsCnt;
+			me.removeByRun = true;
+			me.RemoveEntity();
+
+			requestAnimationFrame(me.Run);
+		}
+	};
+
+	me.Splash = (config) => {
+		if (!me.IsValidObject(me.ctx)) {
+			console.error('Not a valid context for engine');
+			return;
+		}
+
+		let sp = this;
+		sp.duration = 1000;
+		sp.frames = 60;
+		sp.timed = 0;
+		sp.splashReady = false;
+
+		sp.RunSplash = () => {
+			let perc = sp.timed / sp.frames;
+
+			sp.Draw(me.ctx, perc);
+			if (sp.timed != sp.frames) {
+				requestAnimationFrame(sp.RunSplash);
+				++sp.timed;
+				++me._fpsCurr;
+			} else {
+				me.splashDone = true;
+
+			}
+		};
+
+		sp.Draw = (ctx, perc) => {
+			let oldStyle = ctx.fillStyle;
+			let oldFont = ctx.font;
+
+			let msg = "Welcom to Canven";
+
+			ctx.font = "30pt Georgia";
+			let size = ctx.measureText(msg);
+			let x = me.size.x / 2 - (size.width / 2);
+			let y = me.size.y / 2;
+
+			ctx.fillStyle = "#000000";
+			ctx.fillRect(0, 0, me.size.x, me.size.y);
+			ctx.fillStyle = "#ffffff";
+			ctx.fillText(msg, x, y);
+
+			ctx.font = "14pt Georgia";
+			msg = "Programmed by Leland Ede";
+			size = ctx.measureText(msg);
+			x = (me.size.x / 2) - (size.width / 2);
+			y += 30;
+			ctx.fillText(msg, x, y);
+
+			let rgb = me.HexToRGB(me.clearClr);
+			
+			ctx.fillStyle = `rgba(${rgb.toString()},${perc})`;
+			ctx.fillRect(0, 0, me.size.x, me.size.y);
+
+			ctx.font = oldFont;
+			ctx.fillStyle = oldStyle;
+		};
+
+
+		Object.assign(sp, config);
+		sp.Draw(me.ctx, 0);
+		setTimeout(sp.RunSplash, 3000);
+	};
+
+	me.toString = () => {
+		let str = "Canven: {\r\n";
+		let keys = Object.keys(me);
+		for (let i = 0; i < keys.length; ++i) {
+			let k = keys[i];
+			//if (k[0] == '_') continue;
+			if (typeof (me[k]) == 'function') continue;
+			str += `\t[${k}]=${me[k]}\r\n`;
+		}
+		str += "}\r\n";
+
+		return str;
+	};
+
+	// Apply config settings
+	Object.assign(me, config);
+
+	function Collider(config) {
+		let coll = this;
+
+		coll.entity = null;
+
+		coll.Collision = (you) => {
+			console.error("Collision happend so handle it!");
+		};
+
+		coll.CheckHit = (you) => {
+			console.error("Checking for hit to bad we don't have and logic for this!");
+		};
+
+		Object.assign(coll, config);
+	}
+
+	function Entity(config) {
+		let ent = this;
+		ent.id = -1;
+		ent.collider = null;
+		ent.children = new Array();
+		ent.name = "Entity";
+
+		ent.Position = me.NewVector2D(0, 0);
+		ent.Rotation = me.NewVector2D(0, 0);
+		ent.Scale = me.NewVector2D(1, 1);
+		ent.Velocity = me.NewVector2D(0, 0);
+
+		ent.AddChild = (child) => {
+			let chld = ent.children;
+			let id = `${me.id}:${chld.length}`;
+			child.Init({ id: id });
+			chld[chld.length] = child;
+
+			return ent;
+		};
+
+		ent.Draw = (ctx) => {
+			console.error("No defined draw method for entity");
+
+			return ent;
+		}
+
+		ent.Move = (deltaTime) => {
+			ent.Position.x += ent.Velocity.x * deltaTime;
+			ent.Position.y += ent.Velocity.y * deltaTime;
+			return ent;
+		};
+
+		ent.Init = (config) => {
+			Object.assign(this, config);
+
+			return ent;
+		};
+
+		Object.assign(ent, config);
+	}
+
+	function Vector2D(config) {
+		let v = this;
+		v.x = 0;
+		v.y = 0;
+		Object.assign(v, config);
+
+		v.toString = () => {
+			return `[${v.x}, ${v.y}]`;
+		};
+	};
+
+};
---
+++ js/demogame.js
@@ -0,0 +1,246 @@
+﻿/**
+	Issues that need to be debug:
+	1. When the ball hits the wall every once in a while it sticks to the wall
+		Fixed: The reason this happened was because the ball would be in a position where
+		the x flipping would occure every frame so it seemed not to move.
+	2. When balls are being removed from the scene multiple balls will be removed at the same time
+		
+	3. When the ball hits the ground some times the y velocity is removed even though there should still be bouncing
+		Fixed: This was the same issue that happened with the x bounderies.  Now when velocity is flipped the y position
+		is set to the ground plane.
+	4. Now the balls don't stop bouncing.
+		Fixed: The frame count was not high enough to accoung for the low bounce frame count.
+	5. When a ball is suppose to be removed it still is being processed.
+		Fixed: The remove process needed to be cleaned up to only remove items when the engine calls them so that
+		potentially we don't remove an item that is in the process of being drawn.
+*/
+
+
+
+// First thing I need to do is create an event for the site when it is complete
+// There are multiple ways to do this but for today I'm going to be lazy and use jQuery
+$(document).ready(function () {
+	// Before we initialize the engine we need to create a config object
+	let cfg = {
+		clearClr: '#202020',
+		canvasId: 'theCanvas'
+	};
+	// Now when the page is done loading it is time to initialize the game engine
+	let engine = new Canven(cfg);
+
+	// Need a new entity object we can use to add into the game engine
+	function GravBall(config) {
+		// Create a config for the Entity class
+		let ball = engine.NewEntity({
+			name: "Gravity Ball",
+			Calculate: () => {
+				// Setup the needed values for the rise over run formula
+				let delta = engine.NewVector2D(0, 0);
+				// Need the differences of each value in the vector/point
+				delta.x = ball.Position.x - ball.Target.x;
+				delta.y = ball.Position.y - ball.Target.y;
+				let numerator = Math.abs(delta.x) + Math.abs(delta.y);
+
+				// Set initial velocity for the ball
+				ball.Velocity.x = (delta.x / numerator) * ball.speed;
+				ball.Velocity.y = (delta.y / numerator) * ball.speed;
+			},
+			Draw: (ctx) => {
+				// ctx is provided by the game engine when the draw
+				// routine is called.
+
+				// Save the ctx fill style before we wack it out
+				let oldStyle = ctx.fillStyle;
+				let b = ball; // Add this so the arc command is on one line
+
+				// Convert ball color into RGB
+				let rgb = engine.HexToRGB(ball.color);
+
+				ctx.fillStyle = `rgba(${rgb.toString()},${ball.alpha}`;
+				ctx.beginPath();
+				ctx.arc(b.Position.x, b.Position.y, b.ballSize, 0, 2 * Math.PI);
+				ctx.fill();
+
+				// Return the style back to original value before I wakced it
+				ctx.fillStyle = oldStyle;
+			},
+			// Now then this is all great and all but how do we make the ball move?
+			Move: (deltaTime) =>{
+				// I don't like these but I'm to lazy to refactor right now
+				if (!ball.isMoving) return;
+
+				let ground = engine.size.y - 60;
+				// Just change the position based on the entoty velocity
+				ball.Position.x += ball.Velocity.x * deltaTime;
+				ball.Position.y += ball.Velocity.y * deltaTime;
+				// Adjust the velocity by my FAKE gravity
+				//*
+				ball.Velocity.y += ball.gravity;
+
+				// Need to add in a ground layer collision detection
+				if (ball.Position.y > ground) {
+					if (ball.frameCnt > 18) {
+						ball.Velocity.y *= -0.8 // Just an estimate for now
+					} else {
+						// Fix the gitter after physics has depleted the y energy
+						ball.Velocity.y = 0
+					}
+					ball.Position.y = ground;
+					ball.frameCnt = 0;
+				}
+
+				// Ball went bye bye :(
+				if (ball.Position.x > engine.size.x - 10 || ball.Position.x < 10) {
+					// Need to flip x velocity
+					ball.Velocity.x *= -0.7;
+					if (ball.Position.x > engine.size.x - 10) {
+						ball.Position.x = engine.size.x - 10;
+					} else {
+						ball.Position.x = 10;
+					}
+
+					
+				}
+
+				// Slow down ball when on the ground
+				if (ball.Position.y == ground && ball.Velocity.x != 0) {
+					// So we need to always subtract the friction from velocity
+					ball.Velocity.x -= ball.ballFriction
+						* (ball.Velocity.x / Math.abs(ball.Velocity.x));
+					// Clean up the velocity and make it stop moving
+					if (Math.abs(ball.Velocity.x) < 0.01) {
+						ball.Velocity.x = 0;
+					}
+				}
+				// Adding a kill ball and remove from simulation process
+				if (ball.Velocity.x == 0 && ball.Velocity.y == 0) {
+					ball.isMoving = false;
+					setTimeout(ball.RemoveBall, 1000);
+				}
+
+				++ball.frameCnt;
+				//*/
+			},
+			RemoveBall: () => {
+				// Not perfect but it is getting there
+				// It would be nice to fade ball out instead of doing a harsh rip out of game engine
+				let steps = 1 / 60; // 1.0 divided by 60 frames
+				ball.alpha -= steps;
+				if (ball.alpha > 0) {
+					setTimeout(ball.RemoveBall, 1000 / 60);
+				} else {
+					// This method doesn't exists so time to make it
+					engine.RemoveEntity(ball);
+				}
+			},
+			alpha: 1.0,
+			ballFriction: 0.007,
+			ballSize: 10,
+			color: '#ffffff',
+			frameCnt: 0,
+			gravity: 0.2,
+			isMoving: true,
+			speed: -15
+		});
+
+		// Time to merge the config with the new ball entity
+		Object.assign(ball, config);
+		Object.assign(this, ball);
+	};
+
+	function BallCollider(config) {
+		let coll = engine.NewCollider({
+			Collision: (you) => {
+				// Need to figure out what to do when another ball hits me
+				let dirChange = -1;
+				let youPos = you.Position;
+				let mePos = coll.entity.Position;
+				let vel = coll.entity.Velocity;
+
+				let delta = engine.NewVector2D(0, 0);
+				delta.x = youPos.x - mePos.x;
+				delta.y = youPos.y - mePos.y;
+				let numerator = Math.abs(delta.x) + Math.abs(delta.y);
+
+				vel.x *= dirChange;
+				vel.y *= dirChange;
+			},
+			CheckHit: (you) => {
+				// Setup the math variable we need
+				let youPos = you.Position;
+				let mePos = coll.entity.Position;
+				let youRaidus = you.ballSize;
+				let meRadius = coll.entity.ballSize;
+
+				// Calculate the square of the two lines
+				let diff = (Math.abs(mePos.x - youPos.x) * Math.abs(mePos.x - youPos.x)) +
+					(Math.abs(mePos.y - youPos.y) * Math.abs(mePos.y - youPos.y));
+				// Calculate the square of the radius
+				let radius = (youRaidus + meRadius) * (Math.abs(mePos.y - youPos.y));
+
+				return diff < radius;
+			},
+			radius: 0
+		});
+
+		Object.assign(coll, config);
+
+		return coll;
+	}
+
+	// Adding a visual ground plane for the game world
+	function Ground(config) {
+		let grd = engine.NewEntity({
+			color: '#814607',
+			Draw: (ctx) => {
+				let pos = grd.Position;
+				let oldStyle = ctx.fillStyle;
+
+				ctx.fillStyle = grd.color;
+				ctx.fillRect(0, engine.size.y - 50, engine.size.x, engine.size.y);
+
+				ctx.fillStyle = oldStyle;
+			}
+		});
+
+		return grd;
+	}
+
+	// Run the initialization part of the engine
+	engine.Init();
+
+
+	// I want to shoot a ball now!!!!
+	let cv = document.getElementById(engine.canvasId);
+	cv.onclick = (e) => {
+		// Get the event object
+		e = e || windows.event;
+		let clickPos = engine.NewVector2D(e.clientX, e.clientY);
+		// Add this so we can debug the start position
+		let startPos = engine.NewVector2D((engine.size.x / 2) - 5, engine.size.y - 100);
+		// Need to create a new ball each time
+		let newball = new GravBall({
+			Position: startPos,
+			Target: clickPos,
+		});
+		newball.collider = engine.NewCollider(new BallCollider({ entity: newball }));
+		// Need to calculate the velocity as it is shot to the click position
+		newball.Calculate();
+		// Need to add the ball to the simulator or noting will happen duh!?!?
+		engine.AddEntity(newball);
+		console.log(newball);
+	}
+
+	window.onkeydown = (e) => {
+		e = e || windows.event;
+		if (e.which == 81) {
+			engine.Close();
+		}
+	};
+
+	let ground = new Ground({});
+	engine.AddEntity(ground);
+
+	// Run the simulation
+	engine.Run();
+});
